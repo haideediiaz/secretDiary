7c7371d457b065cc43fda5b82c86da9427f94789
haidee <haidee.220607@gmail.com>
2026-08-26 23:15:17 +0200

    login register

--- backend/app.py
+++ backend/app.py
@@ -16,10 +16,10 @@ def register():
     password = data.get('password')
 
     # 1. Comprobar si el usuario ya existe
-    if(User.query.filter_by(username=username).first()):
+    if User.query.filter_by(username=username).first():
         return {"error": "Ya existe"}, 400 # 2. Si existe, devolver un error
 
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') # 3. Hashear la contraseña
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') # 3. Hashear la contra
     # 4. Crear el usuario y guardarlo
     new_user = User(username = username, password = hashed_password)
     db.session.add(new_user)
@@ -35,15 +35,10 @@ def login():
     username_found = User.query.filter_by(username = username).first()
     if not username_found:
         return {"error" : "usuario no encontrado"}, 401
-    if (bcrypt.check_password_hash(username_found.password, password)):
+    if bcrypt.check_password_hash(username_found.password, password):
         return {"exito" : "inicio de sesion correcto"}
     else:
         return {"error" : "contrasenya incorrecta"}, 401
-    
-
-    
-
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
